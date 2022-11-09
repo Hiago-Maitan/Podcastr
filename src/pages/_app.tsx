557665f9454cad7112b97af993bdd28dfa47a104
Maitann <hiagomaitan@gmail.com>
2022-11-09 01:23:11 -0300

    add depencies

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import '../styles/global.scss';
+
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player/>
+    </div>
+  )
+}
